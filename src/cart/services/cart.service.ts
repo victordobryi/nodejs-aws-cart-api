db79f92868229364385fad1d6b4206d68c359bbd
bazla <dobr1nya@mail.ru>
2023-12-25 02:11:50 +0300

    feat: create standalone application (no http server)

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -2,24 +2,30 @@ import { Injectable } from '@nestjs/common';
 
 import { v4 } from 'uuid';
 
-import { Cart } from '../models';
+import { Cart, CartStatuses } from '../models';
 
 @Injectable()
 export class CartService {
   private userCarts: Record<string, Cart> = {};
 
   findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+    return this.userCarts[userId];
   }
 
   createByUserId(userId: string) {
     const id = v4();
-    const userCart = {
+    const now = new Date().toISOString();
+
+    const userCart: Cart = {
       id,
+      user_id: userId,
+      created_at: now,
+      updated_at: now,
+      status: CartStatuses.OPEN,
       items: [],
     };
 
-    this.userCarts[ userId ] = userCart;
+    this.userCarts[userId] = userCart;
 
     return userCart;
   }
@@ -40,16 +46,15 @@ export class CartService {
     const updatedCart = {
       id,
       ...rest,
-      items: [ ...items ],
-    }
+      items: [...items],
+    };
 
-    this.userCarts[ userId ] = { ...updatedCart };
+    this.userCarts[userId] = { ...updatedCart };
 
     return { ...updatedCart };
   }
 
   removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+    this.userCarts[userId] = null;
   }
-
 }
